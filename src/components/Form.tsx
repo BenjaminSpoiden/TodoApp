759f40f823a8f8c1c04f2a9df461cf6b03602dcc
BenjaminSpoiden <spoiden.benjamin@hotmail.com>
2020-09-08 18:20:54 +0200

    refreshing state when clickin on +

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,33 @@
 import React from "react";
 
 const Form = (props: any) => {
-
   const inputTextHandler = (event: any) => {
     console.log(event.target.value);
-    props.setInputText(event.target.value)
-  }
+    props.setInputText(event.target.value);
+  };
+
+  const submitTodosHandler = (event: any) => {
+    event.preventDefault();
+    props.setTodos([
+      ...props.todos,
+      { text: props.inputText, completed: false, id: Math.random() * 1000 },
+    ]);
+    props.setInputText("");
+  };
 
   return (
     <form>
-      <input onChange={inputTextHandler} type="text" className="todo-input" />
-      <button className="todo-button" type="submit">
+      <input
+        value={props.inputText}
+        onChange={inputTextHandler}
+        type="text"
+        className="todo-input"
+      />
+      <button
+        onClick={submitTodosHandler}
+        className="todo-button"
+        type="submit"
+      >
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
